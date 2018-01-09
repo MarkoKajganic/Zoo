f62c14c5e22b6c85d4d9595a973592014af00050
Marko Kajganic <kajganicmarko@gmail.com>
2018-01-09 19:42:23 +0100

    zadatak1

---
+++ src/app/animal-list/animal-list.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-animal-list',
+  templateUrl: './animal-list.component.html',
+  styleUrls: ['./animal-list.component.css']
+})
+export class AnimalListComponent implements OnInit {
+
+  animals: Array<Object>;
+
+  constructor() { 
+    this.animals = [     
+    {
+      species:'Dog',
+      name:'Courage',
+      dateOfBirth:'2015'
+    },
+    {
+      species:'Cat',
+      name:'VivifyCat',
+      dateOfBirth:'2016'
+    },
+    {
+      species:'Snake',
+      name:'Cobra',
+      dateOfBirth:'2002'
+    },
+    {
+      species:'Mouse',
+      name:'shomi',
+      dateOfBirth:'2017'
+    },
+    {
+      species:'Bird',
+      name:'Tica',
+      dateOfBirth:'2011'
+    },
+   
+   ];
+ }
+
+  ngOnInit() {
+  }
+
+}
